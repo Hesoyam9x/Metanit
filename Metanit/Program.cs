b1c9d0be7b432606b3d62643daf67b1cbf6d4a88
LS <linarsaetgaraev@icloud.com>
2022-09-02 14:50:58 +0500

    Добавьте файлы проекта.

---
+++ Metanit/Program.cs
@@ -0,0 +1,181 @@
+using System.Text.RegularExpressions;
+
+// начальные данные
+List<Person> users = new List<Person>
+{
+new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 37 },
+new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 41 },
+new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
+};
+/*
+    Стоит обратить внимание, что каждый объект Person имеет свойство Id, 
+    которое в качестве значения получает Guid - уникальный идентификатор, 
+    например "2e752824-1657-4c7f-844b-6ec2e168e99c".
+
+    Для упрошения данные определены в виде обычного списка объектов, 
+    но в реальной ситуации обычно подобные данные извлекаются из какой-нибудь 
+    базы данных.
+*/
+
+
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.Run(async (context) =>
+{
+    var response = context.Response;
+    var request = context.Request;
+    var path = request.Path;
+    //string expressionForNumber = "^/api/users/([0 - 9]+)$";   // если id представляет число
+
+    // 2e752824-1657-4c7f-844b-6ec2e168e99c
+    string expressionForGuid = @"^/api/users/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$";
+    /*
+        В методе app.Run() определяем компонент middleware, 
+        который в зависимости от типа запросов (GET/POST/PUT/DELETE) выполняет 
+        те или иные действия.
+    */
+    if (path == "/api/users" && request.Method == "GET")
+    {
+        await GetAllPeople(response);
+    }
+    /* 
+    Запрос GET предполагает получение объектов, и в данном случае 
+    отправляем выше определенный список объектов Person.
+
+    Когда клиент обращается к приложению для получения одного объекта по id 
+    в запрос типа GET по адресу "api/users/[id]", то срабатывает следующий код:
+    */
+    else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "GET")
+    {
+        // получаем id из адреса url
+        string? id = path.Value?.Split("/")[3];
+        await GetPerson(id, response);
+    }
+    else if (path == "/api/users" && request.Method == "POST")
+    {
+        await CreatePerson(response, request);
+    }
+    else if (path == "/api/users" && request.Method == "PUT")
+    {
+        await UpdatePerson(response, request);
+    }
+    else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "DELETE")
+    {
+        string? id = path.Value?.Split("/")[3];
+        await DeletePerson(id, response);
+    }
+    else
+    {
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync("html/index.html");
+    }
+});
+
+app.Run();
+
+// получение всех пользователей
+async Task GetAllPeople(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(users);
+}
+// получение одного пользователя по id
+async Task GetPerson(string? id, HttpResponse response)
+{
+    // получаем пользователя по id
+    Person? user = users.FirstOrDefault((u) => u.Id == id);
+    // если пользователь найден, отправляем его
+    if (user != null)
+        await response.WriteAsJsonAsync(user);
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+    }
+}
+
+async Task DeletePerson(string? id, HttpResponse response)
+{
+    // получаем пользователя по id
+    Person? user = users.FirstOrDefault((u) => u.Id == id);
+    // если пользователь найден, удаляем его
+    if (user != null)
+    {
+        users.Remove(user);
+        await response.WriteAsJsonAsync(user);
+    }
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+    }
+}
+
+async Task CreatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        // получаем данные пользователя
+        var user = await request.ReadFromJsonAsync<Person>();
+        if (user != null)
+        {
+            // устанавливаем id для нового пользователя
+            user.Id = Guid.NewGuid().ToString();
+            // добавляем пользователя в список
+            users.Add(user);
+            await response.WriteAsJsonAsync(user);
+        }
+        else
+        {
+            throw new Exception("Некорректные данные");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+    }
+}
+
+async Task UpdatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        // получаем данные пользователя
+        Person? userData = await request.ReadFromJsonAsync<Person>();
+        if (userData != null)
+        {
+            // получаем пользователя по id
+            var user = users.FirstOrDefault(u => u.Id == userData.Id);
+            // если пользователь найден, изменяем его данные и отправляем обратно клиенту
+            if (user != null)
+            {
+                user.Age = userData.Age;
+                user.Name = userData.Name;
+                await response.WriteAsJsonAsync(user);
+            }
+            else
+            {
+                response.StatusCode = 404;
+                await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+            }
+        }
+        else
+        {
+            throw new Exception("Некорректные данные");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+    }
+}
+public class Person
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
